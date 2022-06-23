606d17bd38fc9dd3cc4a351d481ad310919f3c50
AnhLe3698 <anhviet.le00@hotmail.com>
2022-06-23 14:59:45 -0600

    function practice files

---
+++ hello.js
@@ -0,0 +1,11 @@
+const sayHello = function (name){
+  return "Hello, " + name;
+}
+
+let greetings1 = sayHello("Caliban");
+let greetings2 = sayHello("Miranda");
+let greetings3 = sayHello("Ferdinand");
+
+console.log(greetings1);
+console.log(greetings2);
+console.log(greetings3);
