08599db37244f48b42fa75bdb29864e70fccd04d
AnhLe3698 <anhviet.le00@hotmail.com>
2022-06-25 05:40:29 -0600

    array excercise

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+let iceCreamFlavoursNew = iceCreamFlavours.slice();
+iceCreamFlavoursNew.push("root beer");
+
+console.log(iceCreamFlavoursNew);
+console.log(iceCreamFlavoursNew[0]);
+console.log(iceCreamFlavoursNew[5]);
+console.log(iceCreamFlavoursNew.length);
+
+
+
